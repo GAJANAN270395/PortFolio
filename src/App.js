b944707ce77e08b87089cbda2870a05aeb89ac9e
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-01-26 14:56:16 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+const App =()=>{
+  return(
+    <div className='outerDiv'>
+     <div className='navigation'>
+  <div className='App-logo'></div>
+     <h3>GAK</h3>
+     </div>
+     <div className="navItem">
+       <a href='#home'>
+         <h1>🏠</h1>
+         <p>Home</p>
+       </a>
+     </div>
+     <div className='navItem'>
+          <a href='#about'>
+            <h1>🧑</h1>
+            <p>About</p>
+          </a>
+        </div>
+        <div className='navItem'>
+          <a href='#resume'>
+            <h1>👨🏽‍🎓</h1>
+            <p>Resume</p>
+          </a>
+        </div>
+        <div className='navItem'>
+          <a href='#projects'>
+            <h1>🛠</h1>
+            <p>Projects</p>
+          </a>
+        </div>
+        <div className='navItem'>
+          <a href='#contactMe'>
+            <h1>📞</h1>
+            <p>Contact Me</p>
+          </a>
+        </div>
     </div>
-  );
+  )
 }
+export default App;
